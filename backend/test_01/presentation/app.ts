fcd634a77cf626c8e8465d8904387bfb85587ec0
R. Walter <133987890+rwcodev@users.noreply.github.com>
2024-08-03 13:22:36 -0600

    Update app.ts
    Update

--- backend/test_01/presentation/app.ts
+++ backend/test_01/presentation/app.ts
@@ -3,9 +3,6 @@ import { AppRoutes } from './presentation/routes';
 import { Server } from './presentation/server';
 import { PostgresDatabase } from "./src/data/postgres/postgres-data";
 
-https://www.nearshorecyber.community/join?invitation_token=f75ae79514f85d789105f2d0aa569d9b208b662d-6417e8e5-8a3d-4da4-ba6a-5bb5fda5668a
- 
-
 (async() => {
     main();
 })();
