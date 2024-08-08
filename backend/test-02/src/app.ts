9a618cdaf67d0eef4e96e89e97341dfc8c02555f
R. Walter <133987890+rwcodev@users.noreply.github.com>
2024-08-07 20:55:33 -0600

    Add files via upload

---
+++ backend/test-02/src/app.ts
@@ -0,0 +1,23 @@
+import { envs } from './config/envs';
+import { Server } from './presentation/server';
+
+
+async function main() {
+
+    const server = new Server({
+        port: envs.PORT,
+    })
+
+    await server.start();
+
+}
+
+
+main();
+
+
+
+
+
+
+
